11eb99dcc4148161dd8d84c419efb8886cbc15e6
tirtahakimpambudhi <awebthp@gmail.com>
2023-08-29 19:30:55 +0700

    chore: Add app.php files

---
+++ bootstrap/app.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Middleware\DemoMiddleware;
+use App\Http\Middleware\DemoMiddlewareParameter;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\Middleware\StartSession;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+//        $middleware->alias(aliases: ['demo' => DemoMiddleware::class]);
+//        $middleware->alias(aliases: ['demoParam:admin' => DemoMiddlewareParameter::class]);
+        $middleware->appendToGroup('api', StartSession::class);
+        $middleware->appendToGroup('demoGroup',[DemoMiddleware::class,DemoMiddlewareParameter::class . ':admin']);
+        $middleware->encryptCookies(except: [
+            'unencrypted',
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
