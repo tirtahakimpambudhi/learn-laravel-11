4791c4f646119d834e6810871ca8c2bc67871f60
tirtahakimpambudhi <awebthp@gmail.com>
2023-11-02 19:52:38 +0700

    chore: Update DemoSessionTest.php files

--- tests/Feature/DemoSessionTest.php
+++ tests/Feature/DemoSessionTest.php
@@ -32,4 +32,81 @@ it('cannot get session data', function () {
     $response->assertStatus(Response::HTTP_NOT_FOUND);
 });
 
-it('');
+it('can store session data', function () {
+    $id = Str::uuid()->toString();
+    $data = [
+        $id => [
+            'name' => fake()->name,
+            'age' => fake()->randomDigitNotZero()
+        ]
+    ];
+    $response = $this->withSession($data)->post($this->basePath . "/", [
+        'name' => fake()->name,
+        'age' => fake()->randomDigitNotZero()
+    ]);
+    $response->assertStatus(Response::HTTP_OK);
+    $actualData = collect(json_decode($response->getContent(), true));
+    $this->assertNotEmpty($actualData->get('data'));
+});
+
+it('cannot store session data', function () {
+    $id = Str::uuid()->toString();
+    $data = [
+        $id => [
+            'name' => fake()->name,
+            'age' => fake()->randomDigitNotZero()
+        ]
+    ];
+    $response = $this->withSession($data)->post($this->basePath . "/", [
+    ]);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+});
+
+it('can update session data', function () {
+    $id = Str::uuid()->toString();
+    $data = [
+        $id => [
+            'name' => fake()->name,
+            'age' => fake()->randomDigitNotZero()
+        ]
+    ];
+    $response = $this->withSession($data)->put($this->basePath . "/$id", [
+        'name' => fake()->name,
+        'age' => fake()->randomDigitNotZero()
+    ]);
+    $response->assertStatus(Response::HTTP_OK);
+    $actualData = collect(json_decode($response->getContent(), true));
+    $this->assertNotEmpty($actualData->get('data'));
+});
+
+it('cannot update session data', function () {
+    $id = Str::uuid()->toString();
+    $response = $this->withSession([])->put($this->basePath . "/$id", [
+        'name' => fake()->name,
+        'age' => fake()->randomDigitNotZero()
+    ]);
+    $response->assertStatus(Response::HTTP_NOT_FOUND);
+});
+
+
+it('can delete session data', function () {
+    $id = Str::uuid()->toString();
+    $data = [
+        $id => [
+            'name' => fake()->name,
+            'age' => fake()->randomDigitNotZero()
+        ]
+    ];
+    $response = $this->withSession($data)->delete($this->basePath . "/$id");
+    $response->assertStatus(Response::HTTP_OK);
+    $actualData = collect(json_decode($response->getContent(), true));
+    $this->assertEmpty($actualData->get('data'));
+});
+
+it('cannot delete session data', function () {
+    $id = Str::uuid()->toString();
+    $response = $this->withSession([])->delete($this->basePath . "/$id");
+    $response->assertStatus(Response::HTTP_NOT_FOUND);
+});
+
+
