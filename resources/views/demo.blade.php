bad329b6f7e65ce3a960b240f43ab149a9448cd2
tirtahakimpambudhi <awebthp@gmail.com>
2025-05-03 21:18:43 +0700

    chore: Update demo.blade.php files

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -2,6 +2,7 @@
     <x-slot:title>
         Demo View Page
     </x-slot:title>
+    <h1> With Empty Statement</h1>
     @if(!empty($data))
         <h1>Path : {{$data['path'] ?? "/"}}</h1>
         <hr>
@@ -10,4 +11,16 @@
         <p>IP : {{$data['ip'] ?? "127.0.0.1"}}</p>
         <p>User Agent : {{$data['userAgent'] ?? "Symfony"}}</p>
     @endif
+    <h1> With Isset Statement</h1>
+    @isset($data)
+        <h1>Path : {{$data['path'] ?? "/"}}</h1>
+        <hr>
+        <p>Method : {{$data['method'] ?? "GET"}}</p>
+        <p>Full Url : {{$data['fullUrl'] ?? "http://localhost/"}}</p>
+        <p>IP : {{$data['ip'] ?? "127.0.0.1"}}</p>
+        <p>User Agent : {{$data['userAgent'] ?? "Symfony"}}</p>
+    @endisset
+    @empty($data)
+        <h4>Oops the data is empty</h4>
+    @endempty
 </x-base>
